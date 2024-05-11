bb7b8d428587f0862732ce753aac1e1e82dac73d
gamze <gamzekandara@gmail.com>
2024-05-10 22:01:58 -0400

    Adds person.py and dog.py

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+
+new_dog = Dog("chula")
+print(new_dog)
+print(new_dog.name)
+print(new_dog.breed)
+
+new_dog.breed = "terrier"
+
+print(new_dog.breed)
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    # init method runs whenever we instantiate a new Person object. 
+    def __init__(self, name):
+        self.name = name  #binding name argument to self 
+
+
+new_person = Person("John")
+print(new_person)
+        
